7f3d7c4c6d35b436ea6f1d5c9d8f134c696742a8
llima2 <llima2@uolinc.com>
2023-08-04 15:13:20 -0300

    feat: configura uso de assets estaticos

--- setup/settings.py
+++ setup/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, "setup.static"),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "setup/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
