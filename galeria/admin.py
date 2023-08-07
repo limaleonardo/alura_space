eff193989970ea56946ef98ad165e2cd5e4d7dc8
llima2 <llima2@uolinc.com>
2023-08-07 12:35:31 -0300

    feat: melhora declaração de imagens e adiciona funcionalidade de upload no painel do admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,19 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = (
+        "id",
+        "data_fotografia",
+        "nome",
+        "categoria",
+        "publicada",
+        "legenda",
+    )
+    list_editable = ("publicada",)
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 
 admin.site.register(Fotografia, ListandoFotografias)
