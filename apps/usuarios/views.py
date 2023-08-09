6a41ded74b4b5405dd2000fcae6229b3f996c078
llima2 <llima2@uolinc.com>
2023-08-09 15:01:31 -0300

    start: sincroniza templates html com os do inicio do curso para maior facilidade

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, redirect
-
-from usuarios.forms import LoginForms, CadastroForms
-
 from django.contrib.auth.models import User
-
+from django.contrib import messages
 from django.contrib import auth
 
-from django.contrib import messages
+from apps.usuarios.forms import LoginForms, CadastroForms
 
 
 def login(request):
